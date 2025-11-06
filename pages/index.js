5286e61849b19d094777f712f825b926b624d006
Mateus Alves VIeira <156152302+NeonEyesz@users.noreply.github.com>
2025-11-06 15:46:30 +0000

    alteração

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Isabelle, eu amo você. Se você me ama, dá uma risadinha!</h1>
+    return <h1>Isabelle, eu amo você. Se você me ama, dá uma risadinha!!!
+        Muito mesmo!
+    </h1>
 }
 
 export default Home;
