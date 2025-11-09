1407b07f6ded0ca487a120aaaab5e9bc697326c6
Mateus Alves VIeira <156152302+NeonEyesz@users.noreply.github.com>
2025-11-09 13:27:40 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 function Home() {
-    return <h1>Isabelle, eu amo você. Se você me ama, dá uma risadinha!!! Muito mesmo!
+  return (
+    <h1>
+      Isabelle, eu amo você. Se você me ama, dá uma risadinha!!! Muito mesmo!
     </h1>
+  );
 }
 
 export default Home;
-
